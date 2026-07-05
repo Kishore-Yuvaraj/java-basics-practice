b946a13b1d6ca82bdc62384e37fe6bcc6f40d0ab
Kishore-Yuvaraj <y.kishoreyuvaraj@gmail.com>
2026-07-05 20:33:27 +0530

    Lesson5: do-while loop Q3 3-Option Menu using switch case

--- src/lesson5/dowhileloop/Q3OptionMenu.java
+++ src/lesson5/dowhileloop/Q3OptionMenu.java
@@ -4,33 +4,34 @@
 package lesson5.dowhileloop;
 import java.util.Scanner;
 public class Q3OptionMenu {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
         do {
-            System.out.println("1) Square");
-            System.out.println("2) Cube");
-            System.out.println("3) Exit");
-            System.out.print("Enter your Choice: ");
-            n=sc.nextInt();
-            if (n == 1 || n == 2) {
-                System.out.print("Enter a Number: ");
-                int a = sc.nextInt();
-
-                if (n == 1) {
-                    System.out.println(a * a);
-                } else {
-                    System.out.println(a * a * a);
-                }
+            System.out.println("\n========== MENU ==========");
+            System.out.println("1. Square");
+            System.out.println("2. Cube");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a number: ");
+                    int number1 = sc.nextInt();
+                    System.out.println("Square = " + (number1 * number1));
+                    break;
+                case 2:
+                    System.out.print("Enter a number: ");
+                    int number2 = sc.nextInt();
+                    System.out.println("Cube = " + (number2 * number2 * number2));
+                    break;
+                case 3:
+                    System.out.println("Thank you!");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please enter 1, 2, or 3.");
             }
-            else if (n==3) {
-                System.out.println("Exit");
-            }
-            else {
-                System.out.println("Invalid Choice");
-            }
-
-        }while(n!=3);
+        } while (choice != 3);
         sc.close();
     }
 }
