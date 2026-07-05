3e884db4b8d822821b49820500a6563b429fa1a2
Kishore-Yuvaraj <y.kishoreyuvaraj@gmail.com>
2026-07-05 19:23:38 +0530

    Lesson5: breakandcontinue Q1 First Common Multiple

--- src/lesson5/breakandcontinue/Q1FirstCommonMultiple.java
+++ src/lesson5/breakandcontinue/Q1FirstCommonMultiple.java
@@ -3,17 +3,24 @@
 package lesson5.breakandcontinue;
 import java.util.Scanner;
 public class Q1FirstCommonMultiple {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int m=sc.nextInt();
-        int n= sc.nextInt();
-        int i=1;
-       while (i>0){
-           if(i%m==0 && i%n==0){
-               System.out.println("The Common Division number:" +i);
-               break;
-           }
-           i++;
-       }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter M: ");
+        int m = sc.nextInt();
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        if (m <= 0 || n <= 0) {
+            System.out.println("Please enter positive integers.");
+            return;
+        }
+        int i = 1;
+        while (true) {
+            if (i % m == 0 && i % n == 0) {
+                System.out.println("First Common Multiple: " + i);
+                break;
+            }
+            i++;
+        }
+        sc.close();
     }
 }
