86bd3c0bf9f559f790895b24b70f94bd3be5036f
Kishore-Yuvaraj <y.kishoreyuvaraj@gmail.com>
2026-07-07 22:09:38 +0530

    Lesson5: Do While Loop  Q1 Updated with Comments

--- src/lesson5/dowhileloop/Q1PositiveNumberValidator.java
+++ src/lesson5/dowhileloop/Q1PositiveNumberValidator.java
@@ -9,12 +9,12 @@ public class Q1PositiveNumberValidator {
         int count=0;
         do {
             System.out.println("Enter a number:");
-            n= sc.nextInt();
-            count++;
-            if(n<=0){
+            n= sc.nextInt(); //get user input
+            count++; //update the count value to one after the user input
+            if(n<=0){  // if the n is negative it print the Message
                 System.out.println("No Positive value to proceed.");
             }
-        }while (n<=0);
+        }while (n<=0); //loops works until the n is Negative if the Value is Positive the condition is become false and loop Stops.
         System.out.println("Total attempts: " + count);
         sc.close();
 
